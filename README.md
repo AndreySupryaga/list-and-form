058b90cdbf524e8260e7c9d6af4fd5a68999ca1f
andreys <AndreyS@migfin.com>
2016-11-18 17:51:58 +0200

    Created project structure.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Need perform following commands
+
+Node modules
+```
+npm install
+```
+
+Bower components
+```
+bower install
+```
+
+Build project
+```
+gulp build
+```
+
+Start server
+```
+gulp serve
+```
+
+### All commands for first deployment
+
+```
+npm install && bower install && gulp build && gulp serve
+```
---
+++ src/app/index.config.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .config(config);
+
+    /** @ngInject */
+    function config(toastrConfig, $uibTooltipProvider, $locationProvider) {
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+        // Set options third-party lib
+        $uibTooltipProvider.options({popupDelay: 400});
+        toastrConfig.allowHtml = true;
+        toastrConfig.timeOut = 3000;
+        toastrConfig.positionClass = 'toast-bottom-right';
+        toastrConfig.progressBar = true;
+    }
+
+})();
