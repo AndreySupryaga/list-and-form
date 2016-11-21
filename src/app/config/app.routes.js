0e8175f7cd622cff1f14ab555ade21ed822d3401
andreys <AndreyS@migfin.com>
2016-11-21 19:50:56 +0200

    Added disabling buttons after save  user data.

--- src/app/config/app.routes.js
+++ src/app/config/app.routes.js
@@ -5,19 +5,19 @@
         .config(function ($routeProvider) {
             $routeProvider
                 .when('/users/table', {
-                    templateUrl: '/app/components/user/grid/table/users.table.html',
+                    templateUrl: 'app/components/user/grid/table/users.table.html',
                     controller: 'usersGridCtrl'
                 })
                 .when('/users/tile', {
-                    templateUrl: '/app/components/user/grid/tile/users.tile.html',
+                    templateUrl: 'app/components/user/grid/tile/users.tile.html',
                     controller: 'usersGridCtrl'
                 })
                 .when('/user/edit/:id', {
-                    templateUrl: '/app/components/user/form/user.form.html',
+                    templateUrl: 'app/components/user/form/user.form.html',
                     controller: 'userFormCtrl'
                 })
                 .when('/user/add', {
-                    templateUrl: '/app/components/user/form/user.form.html',
+                    templateUrl: 'app/components/user/form/user.form.html',
                     controller: 'userFormCtrl'
                 })
                 .otherwise({
